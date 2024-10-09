6d8bd86fb20e16782aaf98f0201ec28b17dce9d1
Girmay-1 <girmaykibreab93@gmail.com>
2024-10-08 22:12:44 -0700

    updated environment config java class for loading .env file

--- src/main/java/com/acra/config/EnvConfig.java
+++ src/main/java/com/acra/config/EnvConfig.java
@@ -1,13 +1,31 @@
 package com.acra.config;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import io.github.cdimascio.dotenv.DotenvEntry;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.MapPropertySource;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class EnvConfig {
+
     @Bean
     public Dotenv dotenv() {
         return Dotenv.load();
+    }
+
+    @Bean
+    public MapPropertySource dotenvPropertySource(Dotenv dotenv, ConfigurableEnvironment env) {
+        Map<String, Object> properties = new HashMap<>();
+        for (DotenvEntry entry : dotenv.entries()) {
+            properties.put(entry.getKey(), entry.getValue());
+        }
+        MapPropertySource propertySource = new MapPropertySource("dotenvProperties", properties);
+        env.getPropertySources().addFirst(propertySource);
+        return propertySource;
     }
 }
